5e6e4121bca6e81997baebabeac4fb32d1f09413
Sooraj <soorajnambiar8@gmail.com>
2023-10-01 10:36:43 +0530

    added listings data

--- app.js
+++ app.js
@@ -20,25 +20,33 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/traveller');
 }
 
+app.set("view engine", "ejs")
+
 //  Home Route
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-
-app.get("/testListing", async(req, res) => {
-    let sampleListing = new Listing({
-        title: "My new villa",
-        description: "By the Beach",
-        price: 1200,
-        location: "Calamput, Goa",
-        country: "India",
-    });
-
-    await sampleListing.save();
-    console.log("Sample was saved");
-    res.send("Successful testing")
+app.get("/listings", async(req, res) => {
+    allListings = await Listing.find({});
+    res.render("index.ejs", { allListings });
 });
+
+
+//  //  Test data
+// app.get("/testListing", async(req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new villa",
+//         description: "By the Beach",
+//         price: 1200,
+//         location: "Calamput, Goa",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample was saved");
+//     res.send("Successful testing")
+// });
 
 app.listen(8080, () => {
     console.log("Server is listing to server 8080");
